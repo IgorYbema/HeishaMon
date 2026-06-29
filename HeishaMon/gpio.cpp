962d27b56e05543c2002047c959b4aace9e8e47e
IgorYbema <igor@ergens.org>
2026-06-29 22:23:06 +0200

    Update gpio.cpp

--- HeishaMon/gpio.cpp
+++ HeishaMon/gpio.cpp
@@ -16,6 +16,7 @@ void mqttGPIOCallback(char* topic, char* value) {
   sprintf_P(log_msg, PSTR("GPIO: MQTT message received on subtopic '%s' value '%s'"), topic, value);
   log_message(log_msg);
 #ifdef ESP32
+  log_message(_F("GPIO: Does it even go here?'"));
   if (strcmp_P(topic, PSTR("relay/one")) == 0) {
     log_message(_F("GPIO: MQTT message received 'relay/one'"));
     digitalWrite(relayOnePin,((stricmp((char*)"true", value) == 0) || (stricmp((char*)"on", value) == 0)  || (stricmp((char*)"enable", value) == 0)|| (String(value).toInt() == 1 )));
