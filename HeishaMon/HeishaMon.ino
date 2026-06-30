9db9ca19cc83c72abe4a078d5160a3d74785848c
IgorYbema <igor@ergens.org>
2026-06-30 07:56:08 +0200

    Update HeishaMon.ino

--- HeishaMon/HeishaMon.ino
+++ HeishaMon/HeishaMon.ino
@@ -949,6 +949,13 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
       msg[i] = (char)payload[i];
     }
     msg[length] = '\0';
+
+	//also copy topic because a futher mqtt_publish can overwrite the topic memory location
+    char topicbuf[strlen(topic) + 1];
+    strcpy(topicbuf, topic);
+    topic = topicbuf;
+    char* topic_command = topic + strlen(heishamonSettings.mqtt_topic_base) + 1;
+  
     char* topic_command = topic + strlen(heishamonSettings.mqtt_topic_base) + 1; //strip base plus seperator from topic
     if (strcmp(topic_command, mqtt_send_raw_value_topic) == 0)
     { // send a raw hex string
